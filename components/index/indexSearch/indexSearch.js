fd4d4be35787bf2001db3692581996dffedf68c9
YangYuChan <yuchan_yang@126.com>
2018-10-25 18:23:33 +0800

    首页

---
+++ components/index/indexSearch/indexSearch.js
@@ -0,0 +1,23 @@
+// components/index/search/indexSearch.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    tabBar: ["关注", "推荐", "热榜"]
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
